2ec35825f9875ccee16efab80d22739991652b62
AndresPatino <andrespatino0845@gmail.com>
2024-01-19 21:07:17 -0500

    clase 19/01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy } from '@angular/router'; //ANGULAR IONIC
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { IonicStorageModule } from '@ionic/storage-angular'; //IONIC STORAGE IMPORTACIóN
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],//import ionicstorage
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,76 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements AfterViewInit {
+  swiperParams1 = {
+    slidesPerView: 1,
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 40,
+      slideShadows: true,
+    },
+    
+    on: {
+      init(){
+
+      },
+    }
+  };
+  //<ion-button *ngIf="i==1>Hola mundo</ion-button> <---------realizar validaciones (if-else)
+  //para usar etiquetas es usar [innerHTML]="variable.lonecesario"
+  //git add .
+  //git commit -m "proyecto"
+  //git push -u origin main
+  slides = [
+    {
+      imagePath: '..\\assets\\Slide1.png',
+      title: '¡Bienvenido a IonicApp!',
+      description: 'Descubre experiencias en vivo y funciones emocionantes desde la comodidad de tu dispositivo. Explora, compra y disfruta.',
+    },
+    {
+      imagePath: '..\\assets\\Slide2.png',
+      title: 'Explora eventos de primera clase',
+      description: 'Navega a través de una amplia variedad de eventos en vivo y funciones. Desde conciertos de tus artistas favoritos hasta emocionantes eventos deportivos, hay algo para todos.',
+    },
+    {
+      imagePath: '..\\assets\\Slide3.png',
+      title: '¡Consigue tus boletos en segundos!',
+      description: 'Compra boletos de manera rápida y segura. Accede a asientos exclusivos y descubre ofertas especiales. ¡No te pierdas la oportunidad de vivir experiencias inolvidables!',
+    },
+    {
+      imagePath: '..\\assets\\Slide4.png',
+      title: 'Descubre ofertas especiales',
+      description: 'Explora nuestras ofertas exclusivas y ahorra en la compra de tus boletos. ¡No dejes pasar la oportunidad de disfrutar eventos increíbles a precios increíbles!',
+    },
+    {
+      imagePath: '..\\assets\\Slide5.png',
+      title: 'Mantente actualizado',
+      description: 'Recibe notificaciones sobre nuevos eventos, fechas de preventa y actualizaciones importantes. ¡No te pierdas ninguna oportunidad de vivir experiencias únicas!',
+    },
+    {
+      imagePath: '..\\assets\\Slide6.png',
+      title: 'Fácil y seguro',
+      description: 'Compra tus boletos de manera fácil y segura con nuestro sistema de pago protegido. Garantizamos una experiencia sin complicaciones para que disfrutes al máximo.',
+    },
+  ]
+
+  constructor(private router: Router) { }
+
+  goToIntro(){
+    this.router.navigateByUrl('/home');//Doc: https://ionicframework.com/docs/angular/navigation
+  };
+
+  ngAfterViewInit(): void {
+    const swiperEl = document.querySelector('swiper-container');
+    if(swiperEl)
+    {
+      Object.assign(swiperEl, this.swiperParams1);
+    }
+
+  }
+}
